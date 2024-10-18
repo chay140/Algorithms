51181d9437a824f93b6289001382cc9ba8c50e85
Chay <chaihyunpark140@gmail.com>
2024-10-18 09:31:11 +0900

    [level 0] Title: 각도기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -0,0 +1,10 @@
+function solution(angle) {
+    if (angle === 90) return 2;
+    if (angle === 180) return 4;
+    
+    if (angle < 90) {
+        return 1;
+    } else {
+        return 3;
+    }
+}
