85d21372d9f4edd0242abc995d1351704f7e54b8
Chay <chaihyunpark140@gmail.com>
2024-10-22 09:57:35 +0900

    [level 0] Title: 두 수의 합, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120802. 두 수의 합/두 수의 합.js
+++ 프로그래머스/0/120802. 두 수의 합/두 수의 합.js
@@ -1,3 +1,9 @@
 function solution(num1, num2) {
-    return num1 + num2;
+    // reduce 연습
+    const array = [num1, num2];
+    let answer = array.reduce((accumulator, currentVal) => {
+        return accumulator + currentVal;
+    }, 0);
+    
+    return answer;
 }
