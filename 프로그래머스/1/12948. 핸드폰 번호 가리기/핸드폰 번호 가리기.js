a9cd7b163687a59282f5d874d5f1c8b75a98343f
Chay <chaihyunpark140@gmail.com>
2024-10-22 09:08:53 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,10 +1,4 @@
 function solution(phone_number) {
-    let answer = '';
-    
-    for (let i = 0; i < phone_number.length - 4; i++) {
-        answer += '*'
-    }
-    
-    answer += phone_number.slice(-4);
-    return answer;
+    let answer = phone_number.slice(-4);
+    return answer.padStart(phone_number.length, "*");
 }
