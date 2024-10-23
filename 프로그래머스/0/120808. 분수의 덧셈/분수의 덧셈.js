1df5e9d01dfc506a46037e8526d662ebbb81e8a6
Chay <chaihyunpark140@gmail.com>
2024-10-23 11:41:25 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -2,14 +2,14 @@ function solution(numer1, denom1, numer2, denom2) {
     let numer = numer1 * denom2 + numer2 * denom1;
     let denom = denom1 * denom2;
     
-    let gcd = 1;
-    // 최대 공약수찾기
-    for (let i = 2; i <= Math.min(denom, numer); i++) {
-        if (denom % i === 0 && numer % i === 0) {
-            gcd = i;
+    const findGCD = (a, b) => {
+        while (b !== 0) {
+            [a, b] = [b, a % b];
         }
+        return a
     }
     
-    return [numer / gcd, denom / gcd];
+    const gcd = findGCD(numer, denom);
     
+    return [numer / gcd, denom / gcd];
 }
