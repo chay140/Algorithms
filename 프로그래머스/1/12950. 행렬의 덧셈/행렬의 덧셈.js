db84a21ffaac303d33f353cd70c982c282e4768e
Chay <chaihyunpark140@gmail.com>
2024-10-24 09:41:51 +0900

    [level 1] Title: 행렬의 덧셈, Time: 12.98 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,12 +1,12 @@
 function solution(arr1, arr2) {
-    let answer = [];
+//     let answer = [];
     
-    arr1.forEach((element, i) => {
-        const temp_element = [];
-        element.forEach((number, j) => {
-            temp_element.push(number + arr2[i][j]);
-        });
-        answer.push(temp_element);
-    });
-    return answer;
+//     arr1.forEach((element, i) => {
+//         const temp_element = [];
+//         element.forEach((number, j) => {
+//             temp_element.push(number + arr2[i][j]);
+//         });
+//         answer.push(temp_element);
+//     });
+    return arr1.map((element, i) => element.map((number, j) => number += arr2[i][j]));
 }
