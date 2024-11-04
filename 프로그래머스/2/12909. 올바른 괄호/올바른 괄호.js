ee45bc2010f5e60aee718c2441ab4386284cc4e6
Chay <chaihyunpark140@gmail.com>
2024-11-04 23:53:11 +0900

    [level 2] Title: 올바른 괄호, Time: 49.02 ms, Memory: 37.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,14 @@
+function solution(s){
+    let stack = [];
+    
+    for (let i = 0; i < s.length; i++) {
+        // stack에 추가
+        stack.push(s[i]);
+        
+        if (stack.at(-1) === '\)' && stack.at(-2) === '\(') {
+            stack.pop();
+            stack.pop();
+        }
+    }
+    return stack.length === 0 ? true : false;
+}
