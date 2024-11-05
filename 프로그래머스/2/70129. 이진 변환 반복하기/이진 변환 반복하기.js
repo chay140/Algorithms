aee1e62262cd059627cc2f047065d2109ba8cbfd
Chay <chaihyunpark140@gmail.com>
2024-11-06 00:35:10 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 6.39 ms, Memory: 36.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -0,0 +1,17 @@
+function solution(s) {
+    let steps = 0
+    let zeroes = 0;
+    while (s !== "1") {
+        const ones = s.replaceAll("0",'');
+        // 0 개수 파악
+        zeroes += s.length - ones.length;
+    
+        // 길이 2진법으로 표현
+        s = Number(ones.length).toString(2);
+        
+        steps++;
+    }
+    
+    
+    return [steps, zeroes];
+}
