d5cc7ca3d796f7672510188e4c4a59fd27659ea6
Chay <chaihyunpark140@gmail.com>
2024-11-18 09:59:37 +0900

    [level 2] Title: 숫자의 표현, Time: 0.17 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,9 +1,12 @@
 function solution(n) {
-    let answer = 0
-    
-    for (let i = 1; i <= n; i++) {
-        if (n % i === 0 && i % 2 === 1) answer++;
+    let answer = 0;
+
+    for (let k = 1; k * (k - 1) / 2 < n; k++) {
+        // x = (n - (k * (k - 1)) / 2) / k 정수인 경우만!
+        if ((n - (k * (k - 1)) / 2) % k === 0) {
+            answer++;
+        }
     }
 
-    return answer
+    return answer;
 }
