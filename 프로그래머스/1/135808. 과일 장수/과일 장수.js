f9d39c06b55696b27bb9bded9e00cd9afdb34599
Chay <chaihyunpark140@gmail.com>
2024-11-19 09:35:45 +0900

    [level 1] Title: 과일 장수, Time: 476.96 ms, Memory: 84.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,19 @@
+function solution(k, m, score) {
+    let answer = 0;
+    
+    score.sort((a,b) => b-a);
+    
+    // while (score.length >= m) {
+    //     const box = score.splice(0, m);
+    //     answer += (Math.min(...box) * m)
+    // }
+    
+    score.filter((fruit, index) => {
+        if ((index + 1) % m === 0) {
+            answer += score[index] * m
+        }
+    });
+    
+    
+    return answer;
+}
