62956d4d8a931068cef089f45cc5eb7b0359455c
Chay <chaihyunpark140@gmail.com>
2024-12-08 20:59:07 +0900

    [level 3] Title: 정수 삼각형, Time: 6.60 ms, Memory: 42.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -0,0 +1,25 @@
+function solution(triangle) {
+    let dp_triangle = [];
+    for (let i = 0; i < triangle.length; i++) {
+        let each_row = [];
+        for (let j = 0; j < triangle[i].length; j++) {
+            let temp = -1;
+            // initial case
+            if (i === 0 && j === 0) {
+                temp = triangle[i][j]
+            } else {
+                if (j === 0) {
+                    temp = dp_triangle[i-1][j] + triangle[i][j];
+                } else if (j === triangle[i].length - 1) {
+                    temp = dp_triangle[i-1][j-1] + triangle[i][j];
+                } else {
+                    temp = Math.max(dp_triangle[i-1][j-1], dp_triangle[i-1][j]) + triangle[i][j];
+                }
+            }
+            each_row.push(temp)
+        }
+        // 연산한 값 계산
+        dp_triangle.push(each_row)
+    }
+    return Math.max(...dp_triangle[dp_triangle.length -1]);
+}
