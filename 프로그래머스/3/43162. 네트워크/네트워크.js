8016eb751b450ac22036a83a0dd58f1e8a43fdef
Chay <chaihyunpark140@gmail.com>
2025-04-09 09:46:04 +0900

    [level 3] Title: 네트워크, Time: 0.39 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -0,0 +1,29 @@
+function solution(n, computers) {
+    let answer = 0;
+    let visited = []; // 탐색을 마친 노드들
+
+    const BFS = (startNode) => {
+        let needVisit = []; // 탐색해야할 노드들
+        visited[startNode] = true;
+        needVisit.push(startNode);
+
+        while (needVisit.length !== 0) {
+            const currNode = needVisit.shift(); // 가장 오래 남아있던 정점을 뽑아냄.
+            computers[currNode].forEach((isConnected, nextNode) => {
+                if (!isConnected || visited[nextNode]) return;
+                visited[nextNode] = true;
+                needVisit.push(nextNode)
+            })
+        }
+        return visited;
+    };
+    
+    for (let i = 0; i < n; i++) {
+        if (!visited[i]) {
+            answer ++;
+            BFS(i);
+        }
+    }
+    
+    return answer;
+}
