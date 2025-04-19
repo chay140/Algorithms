6dad569088a75930fc7144dbef9badd21dbe4b4f
Chay <chaihyunpark140@gmail.com>
2025-04-19 23:28:16 +0900

    [level 3] Title: 등굣길, Time: 23.83 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.js
+++ 프로그래머스/3/42898. 등굣길/등굣길.js
@@ -1,5 +1,6 @@
 function solution(m, n, puddles) {
     let dp = Array.from({length: n}, () => new Array(m).fill(0));
+    // 시작점은 루프 돌리기 전에 지정
     dp[0][0] = 1;
 
     // 웅덩이 표시
@@ -9,13 +10,14 @@ function solution(m, n, puddles) {
     
     for (let i = 0; i < dp.length; i++) {
         for (let j = 0; j < dp[i].length; j++) {
+            // 불필요한 연산 건너뛰기
             if (dp[i][j] < 0 || (i === 0 && j === 0)) continue;
             
-            // 왼쪽의 경우의 수
+            // 왼쪽 경우의 수
             if (j - 1 >= 0 && dp[i][j - 1] >= 0) {
                 dp[i][j] += dp[i][j - 1] % 1000000007;
             }
-            // 위쪽의 경우의 수
+            // 위쪽 경우의 수
             if (i - 1 >= 0 && dp[i - 1][j] >= 0) {
                 dp[i][j] += dp[i - 1][j] % 1000000007;
             }
