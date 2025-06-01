da11d8af6eb5d62f688fab9a08025a5e9f838f63
Chay <chaihyunpark140@gmail.com>
2025-06-01 09:40:13 +0900

    [level 2] Title: 프로세스, Time: 0.36 ms, Memory: 32.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -0,0 +1,22 @@
+function solution(priorities, location) {
+    let answer = 0;
+    let wait_queue = Array(priorities.length).fill().map((_, i) => i);
+    
+    while (wait_queue.length > 0) {
+        const process_id = wait_queue.shift();
+        const process_priority = priorities.shift();
+        
+        const max_priority = Math.max(...priorities)
+        
+        if (max_priority > process_priority) {
+            wait_queue.push(process_id);
+            priorities.push(process_priority);
+        } else {
+            answer++;
+            
+            if (process_id === location) break;
+        }
+    }
+    
+    return answer;
+}
