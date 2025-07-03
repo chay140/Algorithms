9362a171367b83f32c1fa61a91e61946c8b9c5c3
Chay <chaihyunpark140@gmail.com>
2025-07-04 03:32:25 +0900

    [level 1] Title: 폰켓몬, Time: 22.12 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,9 +1,10 @@
 function solution(nums) {
     const map = new Map();
     
+    // 총 폰켓몬 종류수 파악
     nums.forEach((num) => {
         map.set(num, (map.get(num) || 0) + 1);
     })
-    
-    return map.size < nums.length / 2 ? map.size : nums.length / 2;
+
+    return Math.min(nums.length / 2, map.size);
 }
