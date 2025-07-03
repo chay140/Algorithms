1b0c606f28f3fbbf8cf0bad2e13fd05e796f23e2
Chay <chaihyunpark140@gmail.com>
2025-07-04 03:42:15 +0900

    [level 2] Title: 전화번호 목록, Time: 131.30 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=javascript) 
 
 ### 성능 요약
 
-메모리: 97.3 MB, 시간: 416.60 ms
+메모리: 75.7 MB, 시간: 131.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 18일 23:14:35
+2025년 07월 04일 03:42:13
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,16 +1,14 @@
 function solution(phone_book) {
-    let answer = true;
-    const set = new Set(phone_book);
+    // 해시 사용 없이 푸는 버전
+    // 정렬
+    phone_book.sort();
     
-    for (let number of set) {
-        for (let i = 0; i < number.length - 1; i++) {
-            const prefix = number.slice(0, i + 1);
-                        
-            if (set.has(prefix)) {
-                answer = false;
-                break;
-            }
+    // 인접한 두 번호 비교 (정렬로 최대한 비슷한 번호들이 인접)
+    for (let i = 0; i < phone_book.length - 1; i++) {
+        if (phone_book[i + 1].startsWith(phone_book[i])) {
+            return false;
         }
     }
-    return answer;
+    
+    return true;
 }
