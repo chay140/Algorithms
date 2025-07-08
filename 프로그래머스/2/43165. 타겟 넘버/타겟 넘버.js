0f0b3ad0f7173e4a0c83191ba61d428b8c534699
Chay <chaihyunpark140@gmail.com>
2025-07-09 02:12:40 +0900

    [level 2] Title: 타겟 넘버, Time: 22.90 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,21 +1,21 @@
 function solution(numbers, target) {
-    let answer = 0;
+    let count = 0;
     
-    const dfs = (index, sum) => {
-        // 재귀 탈출 조건
-        // index 가 numbers.length와 일치
-        if (index === numbers.length) {
-            // 탈출할때 target 달성 여부 체크
-            if (sum === target) answer++;
+    const dfs = (depth, sum) => {
+        // 탈출 조건
+        if (depth === numbers.length) {
+            // 타겟 넘버를 찾은 경우
+            if (sum === target) count++;
             return;
         }
-        // +numbers[index] 혹은 -numbers[index];
-        // 수행 동작
-        dfs(index + 1, sum + numbers[index]);
-        dfs(index + 1, sum - numbers[index]);
+        
+        // +/- 재귀 수행
+        dfs(depth + 1, sum + numbers[depth]);
+        dfs(depth + 1, sum - numbers[depth]);
     }
     
+    // 그래프 돌기 전 depth = 0, sum = 0
     dfs(0, 0);
     
-    return answer;
+    return count;
 }
