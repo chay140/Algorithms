ab1c8fb80aa3c3984dbdfcb9125a4c936e2f5183
Chay <chaihyunpark140@gmail.com>
2025-07-17 12:37:57 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,11 +1,22 @@
 function solution(array) {
-    const map = new Map();
-    
-    for (const item of array) {
-        map.set(item, (map.get(item) || 0) + 1);
+  const map = new Map();
+  let maxFreq = 0;
+
+  for (const item of array) {
+    const freq = (map.get(item) || 0) + 1;
+    map.set(item, freq);
+    if (freq > maxFreq) maxFreq = freq;
+  }
+
+  // maxFreq를 가진 값이 여러 개인지 확인
+  let mode = -1;
+  let count = 0;
+  for (const [num, freq] of map) {
+    if (freq === maxFreq) {
+      count++;
+      mode = num;
     }
-    
-    const sorted_map_to_arr = [...map].sort((a, b) => b[1] - a[1]);
-    
-    return sorted_map_to_arr.length === 1 || sorted_map_to_arr[0][1] > sorted_map_to_arr[1][1] ? sorted_map_to_arr[0][0] : -1;
+  }
+
+  return count === 1 ? mode : -1;
 }
