37fa3aeea70644d1974789762109b992ff373fa8
Chay <chaihyunpark140@gmail.com>
2025-07-18 20:01:44 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,7 +1,4 @@
 function solution(num_list) {
     const [a, b] = num_list.slice(-2);
-    if (a < b) num_list.push(b - a);
-    else if (a >= b) num_list.push(b * 2);
-    
-    return num_list;
+    return [...num_list, a < b ? b - a : b * 2];
 }
