2f6774db3addbf0c72b86fb42f3c692838a6a467
Chay <chaihyunpark140@gmail.com>
2025-07-19 15:24:34 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -0,0 +1,4 @@
+function solution(num_list) {
+    if (num_list.length >= 11) return num_list.reduce((acc, curr) => acc + curr, 0);
+    else return num_list.reduce((acc, curr) => acc * curr, 1);
+}
