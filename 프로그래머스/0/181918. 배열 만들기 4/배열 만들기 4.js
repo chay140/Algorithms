6f9cd30144804757fd466c454edd199d7e919242
Chay Park <chaihyunpark140@gmail.com>
2025-07-20 11:38:43 +0900

    [level 0] Title: 배열 만들기 4, Time: 3.82 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -4,10 +4,8 @@ function solution(arr) {
     let i = 0;
     
     while (i < arr.length) {
-        if (stk.length === 0) {
+        if (stk.length === 0 || stk[stk.length - 1] < arr[i]) {
             stk.push(arr[i++]);   
-        } else if (stk[stk.length - 1] < arr[i]) {
-            stk.push(arr[i++]);
         } else {
             stk.pop();
         }
