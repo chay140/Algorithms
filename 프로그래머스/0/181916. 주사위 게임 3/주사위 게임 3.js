7a2b723c29b81297b8e8df07a4e438f5810577c7
Chay Park <chaihyunpark140@gmail.com>
2025-07-21 03:31:54 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.js
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.js
@@ -6,18 +6,17 @@ function solution(a, b, c, d) {
         map.set(num, (map.get(num) || 0) + 1);
     }
     
-    const sorted_map = new Map([...map.entries()].sort((a, b) => b[1] - a[1]));
-    const entries = [...sorted_map.entries()];
+    const sorted_entries = [...map.entries()].sort((a, b) => b[1] - a[1]);
     
-    if (map.size === 1) return entries[0][0] * 1111;
+    if (map.size === 1) return sorted_entries[0][0] * 1111;
     else if (map.size === 2) {
-        if (entries[0][1] === entries[1][1]) {
-            return ((entries[0][0] + entries[1][0]) * Math.abs(entries[0][0] - entries[1][0]));
-        } else return Math.pow((10 * entries[0][0] + entries[1][0]), 2);
+        if (sorted_entries[0][1] === sorted_entries[1][1]) {
+            return ((sorted_entries[0][0] + sorted_entries[1][0]) * Math.abs(sorted_entries[0][0] - sorted_entries[1][0]));
+        } else return Math.pow((10 * sorted_entries[0][0] + sorted_entries[1][0]), 2);
     } else if (map.size === 3) {
-        return entries[1][0] * entries[2][0];
+        return sorted_entries[1][0] * sorted_entries[2][0];
     } else {
-        entries.sort((a, b) => a[0] - b[0]);
-        return entries[0][0];
+        sorted_entries.sort((a, b) => a[0] - b[0]);
+        return sorted_entries[0][0];
     }
 }
