181ee2425e0ebd13ca70dfe2544b6585b70ca5b8
Chay Park <chaihyunpark140@gmail.com>
2025-07-25 12:29:34 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.03 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,13 +1,3 @@
 function solution(arr, idx) {
-    let answer = -1;
-    
-    while (idx < arr.length) {
-        if (arr[idx] === 1) {
-            answer = idx;
-            break;
-        }
-        idx++;
-    }
-    
-    return answer;
+    return arr.indexOf(1, idx)
 }
