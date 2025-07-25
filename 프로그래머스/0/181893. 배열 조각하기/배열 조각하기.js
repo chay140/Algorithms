47ffb18e6fa7101fd18eb1d03b8f466db50710a7
Chay Park <chaihyunpark140@gmail.com>
2025-07-26 01:08:38 +0900

    [level 0] Title: 배열 조각하기, Time: 1.88 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
@@ -1,11 +1,5 @@
 function solution(arr, query) {
-    query.forEach((val, idx) => {
-        if (idx % 2 === 0) {
-            arr = arr.slice(0, val + 1);
-        } else {
-            arr = arr.slice(val);
-        }
-    })
+    query.forEach((val, idx) => arr = idx % 2 === 0 ? arr.slice(0, val + 1): arr.slice(val));
     
     return arr;
 }
