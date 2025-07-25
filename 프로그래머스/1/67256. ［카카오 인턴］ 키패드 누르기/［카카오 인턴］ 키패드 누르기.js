0d21435da156df4fb013b73805d8a4264b2d0a57
Chay Park <chaihyunpark140@gmail.com>
2025-07-25 19:57:10 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.71 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -0,0 +1,44 @@
+function solution(numbers, hand) {
+    // 거리 구하기
+    // left/right -> 우선순위 설정
+    
+    // curr: string, target: number
+    const getDistance = (curr, target) => {
+        let diff = Math.abs(target - Number(curr));
+        return ~~(diff / 3) + diff % 3;
+    }
+    let answer = ""
+    
+    const curr_hands = {
+        "L": "10",
+        "R": "12"
+    };
+    
+    numbers.forEach(number => {
+        if (number === 0) number = 11;
+
+        let key = ""
+        if (String(number).match(/^([147])$/)) {
+            key = "L"
+        } else if (String(number).match(/^([369])$/)) {
+            key = "R"
+        } else {
+            const left_distance = getDistance(curr_hands["L"], number);
+            const right_distance = getDistance(curr_hands["R"], number);
+            
+            if (left_distance === right_distance) {
+                key = hand[0].toUpperCase();
+            } else {
+                if (left_distance < right_distance) {
+                    key = "L"
+                } else {
+                    key = "R"
+                }
+            }
+        }
+        answer += key;
+        curr_hands[key] = String(number);
+    })
+    
+    return answer;
+}
