76de38ed701dcf0460be1f2214c0e1b97502eaed
Chay Park <chaihyunpark140@gmail.com>
2025-07-26 01:01:37 +0900

    [level 0] Title: 2의 영역, Time: 0.40 ms, Memory: 42.2 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.js
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.js
@@ -2,8 +2,5 @@ function solution(arr) {
     const start = arr.indexOf(2);
     const end = arr.lastIndexOf(2);
     
-    if (start === -1) return [-1];
-    else if (start === end) return [2];
-    
-    return arr.slice(start, end + 1);
+    return start === -1? [-1] : arr.slice(start, end + 1);
 }
