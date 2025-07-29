c3c40c2b1eadae982603abde3cb96b13bed8c812
Chay Park <chaihyunpark140@gmail.com>
2025-07-30 03:20:53 +0900

    [level 0] Title: OX퀴즈, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
@@ -0,0 +1,14 @@
+function solution(quiz) {
+    const answer = [];
+    
+    for (const q of quiz) {
+        const [x, op, y, eq, z] = q.split(" ");
+        
+        const correct = op === "+" ? Number(x) + Number(y) : Number(x) - Number(y);
+        
+        if (correct === Number(z)) answer.push("O");
+        else answer.push("X")
+    }
+    
+    return answer;
+}
