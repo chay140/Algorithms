fa1d12713f02254234f59abb88c02bc43008f753
Chay Park <chaihyunpark140@gmail.com>
2025-08-10 12:05:35 +0900

    [level 2] Title: 피로도, Time: 5.95 ms, Memory: 36.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -0,0 +1,19 @@
+function solution(k, dungeons) {
+    let answer = 0;
+    const visited = new Array(dungeons.length).fill(false);
+    
+    function DFS(hp, count) {
+        for (let i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && dungeons[i][0] <= hp) {
+                visited[i] = true;
+                DFS(hp - dungeons[i][1], count + 1);
+                visited[i] = false;
+            }
+        }
+        answer = Math.max(answer, count);
+    }
+    
+    DFS(k, 0);
+    
+    return answer;
+}
