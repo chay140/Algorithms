8f0176c3185b319869d6661a087968fce5a0e0eb
Chay Park <chaihyunpark140@gmail.com>
2025-08-13 14:03:43 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,8 +1,4 @@
 function solution(common) {
-    const [a, b, c] = common.slice(0, 3);
-    const last_idx = common.length - 1;
-    const diff1 = b - a;
-    const diff2 = c - b;
-    
-    return diff1 === diff2 ? common[last_idx] + diff1 : common[last_idx] * (b / a); 
+    const [a, b, c] = common.slice(0, 3);    
+    return b - a === c - b ? common.pop() + (b - a) : common.pop() * (b / a); 
 }
