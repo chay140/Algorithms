0113b1a8e9ef68894a5a8b899cfcd2ade704c5d4
Chay Park <chaihyunpark140@gmail.com>
2025-08-13 13:29:59 +0900

    [level 0] Title: 문자열 밀기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,11 +1,3 @@
 function solution(A, B) {
-    let count = -1;
-    let copy = [...A];
-    for (let i = 0; i < 100; i++) {
-        if (copy.join("") === B) return i;
-        const last_char = copy.pop();
-        copy.unshift(last_char);
-    }
-    
-    return count;
+    return (B + B).indexOf(A);
 }
