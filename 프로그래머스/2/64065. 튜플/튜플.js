ea8d15691d245f009880c1e58ec57d41931beed8
Chay Park <chaihyunpark140@gmail.com>
2025-08-17 13:38:05 +0900

    [level 2] Title: 튜플, Time: 27.93 ms, Memory: 46.9 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,17 +1,17 @@
 function solution(s) {
     const map = new Map();
     let answer = new Set();
-    
+    // 중괄호 제거
     s = s.slice(2, s.length - 2).split("},{");
-    
+    // 각 집합의 길이가 key, 집합(배열)이 value인 맵 작성
     s.forEach(element => {
         const temp = element.split(",").map(string => Number(string));
         map.set(temp.length, temp);
     })
-    
+    // 작은 key부터 Set + spread 로 중복값 위치 상관 없이 추가
     for (let i = 1; i <= map.size; i++) {
         answer = new Set([...answer, ...map.get(i)]);
     }
-    
+    // 배열로 변환
     return Array.from(answer);
 }
