75fb77976c658fcf0402c333df4453cd352bce01
Chay Park <chaihyunpark140@gmail.com>
2025-08-18 13:49:42 +0900

    [level 3] Title: 네트워크, Time: 0.79 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,26 +1,21 @@
 function solution(n, computers) {
     let answer = 0;
-    let visited = []; // 탐색을 마친 노드들
+    let visited = new Set() // 탐색을 마친 노드들
 
-    const BFS = (startNode) => {
-        let needVisit = []; // 탐색해야할 노드들
-        visited[startNode] = true;
-        needVisit.push(startNode);
-
-        while (needVisit.length !== 0) {
-            const currNode = needVisit.shift(); // 가장 오래 남아있던 정점을 뽑아냄.
-            computers[currNode].forEach((isConnected, nextNode) => {
-                if (!isConnected || visited[nextNode]) return;
-                visited[nextNode] = true;
-                needVisit.push(nextNode)
-            })
+    function dfs (start) {
+        visited.add(start);
+        
+        for (const neighbor in computers) {
+            if (!visited.has(neighbor) && computers[start][neighbor] > 0) {
+                dfs(neighbor);
+            }
         }
     }
     
-    for (let i = 0; i < n; i++) {
-        if (!visited[i]) {
-            answer ++;
-            BFS(i);
+    for (const i in computers) {
+        if (!visited.has(i)) {
+            answer++;
+            dfs(i)
         }
     }
     
