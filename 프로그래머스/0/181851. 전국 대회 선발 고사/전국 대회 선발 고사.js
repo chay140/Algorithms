6f13915f25f4673f3874da8b3e274f8d7791fba2
Chay Park <chaihyunpark140@gmail.com>
2025-08-20 01:14:17 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.25 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -1,13 +1,9 @@
 function solution(rank, attendance) {
-    const three_students = [];
+    const [a, b, c] = rank
+        .map((r, i) => [r, i])
+        .filter(([_, i]) => attendance[i])
+        .sort((a, b) => a[0] - b[0])
+        .slice(0, 3);
     
-    for (let i = 1; i <= rank.length; i++) {
-        const stu_idx = rank.indexOf(i);
-        if (attendance[stu_idx]) three_students.push(stu_idx);
-        
-        if (three_students.length === 3) break;
-    }
-    
-    const [a, b, c] = three_students;
-    return 10000 * a + 100 * b + c;
+    return 10000 * a[1] + 100 * b[1] + c[1];
 }
