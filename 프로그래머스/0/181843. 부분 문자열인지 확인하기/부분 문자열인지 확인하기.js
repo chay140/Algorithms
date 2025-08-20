431c7c21f38e3518619918ce9688ee3becea24f6
Chay Park <chaihyunpark140@gmail.com>
2025-08-21 01:36:23 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.js
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.js
@@ -1,8 +1,3 @@
 function solution(my_string, target) {
-    for (let i = 0; i < my_string.length; i++) {
-        for (let j = i + 1; j <= my_string.length; j++) {
-            if (my_string.substring(i, j) === target) return 1;
-        }
-    }
-    return 0;
+    return +my_string.includes(target);
 }
