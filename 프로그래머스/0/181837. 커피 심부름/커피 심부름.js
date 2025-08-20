695579ad6cdb3895c3dae81dbdc06a0c6ea5b1ba
Chay Park <chaihyunpark140@gmail.com>
2025-08-21 01:57:28 +0900

    [level 0] Title: 커피 심부름, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
@@ -1,12 +1,3 @@
 function solution(order) {
-    const AMERICANO = 4500;
-    const LATTE = 5000;
-    let total = 0;
-    
-    order.forEach(cofee => {
-        if (cofee.includes("latte")) total += LATTE;
-        else total += AMERICANO
-    })
-    
-    return total;
+    return order.reduce((acc, curr) => acc + (curr.includes("latte") ? 5000 : 4500), 0);
 }
