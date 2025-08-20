eaf17cfc38040de3bcd219d1715747a23b55426a
Chay Park <chaihyunpark140@gmail.com>
2025-08-20 15:56:40 +0900

    [level 2] Title: [3차] n진수 게임, Time: 16.71 ms, Memory: 38.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
@@ -0,0 +1,21 @@
+function solution(n, t, m, p) {
+    // 1차 시도 (시간 초과)
+    // 구해야할 전체 개수 m * t
+    let baseStrings = ""
+    
+    let i = 0;
+    while(baseStrings.length < m * t) {
+        const converted = (i).toString(n).toUpperCase();
+        baseStrings += converted
+        i++;
+    }
+    
+    let answer = "";
+    for (let j = 0; j < baseStrings.length; j++) {
+        if ((j + 1) % m === p % m) answer += baseStrings[j];
+        
+        if (answer.length === t) break;
+    }
+    
+    return answer;
+}
