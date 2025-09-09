aad84f1dad49c40769aacb110696e60caf9ce7c2
Chay Park <chaihyunpark140@gmail.com>
2025-09-09 23:11:56 +0900

    [level 2] Title: 주차 요금 계산, Time: 6.20 ms, Memory: 34.5 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -1,18 +1,44 @@
 function solution(fees, records) {
-    const parkingTime = {};
-    records.forEach(r => {
-        let [time, id, type] = r.split(' ');
-        let [h, m] = time.split(':');
-        time = (h * 1) * 60 + (m * 1);
-        if (!parkingTime[id]) parkingTime[id] = 0;
-        if (type === 'IN') parkingTime[id] += (1439 - time);
-        if (type === 'OUT') parkingTime[id] -= (1439 - time);
-    });
-    const answer = [];
-    for (let [car, time] of Object.entries(parkingTime)) {
-        if (time <= fees[0]) time = fees[1];
-        else time = Math.ceil((time - fees[0]) / fees[2]) * fees[3] + fees[1]
-        answer.push([car, time]);
+    // 정렬 차량번호가 작은순
+    records = records.map(r => r.split(" "));
+    records.sort((a, b) => a[1] - b[1]);    // 오름차순
+    
+    // {차량번호: [시각, 시각, 시각, ...]}
+    const car_map = new Map();
+    
+    for (const r of records) {
+        const times = car_map.get(r[1]) || [];
+        times.push(r[0])
+        car_map.set(r[1], times)
     }
-    return answer.sort((a, b) => a[0] - b[0]).map(v => v[1]);
+    
+    // 누적 시간 구하기
+    const total_times = [];
+    // 문자열 변환
+    const str_to_time = (str_time) => {
+        const [hour, min] = str_time.split(":");
+        return Number(hour) * 60 + Number(min);
+    }
+    
+    car_map.forEach(car_in_out => {
+        // 마지막 출차가 없는 경우 23:59를 마지막으로 간주
+        if (car_in_out.length % 2 !== 0) car_in_out.push('23:59');
+        
+        let total_time = 0;
+        for (let i = 0; i < car_in_out.length; i += 2) {
+            const start = str_to_time(car_in_out[i]);
+            const end = str_to_time(car_in_out[i + 1]);
+            const timeDiff = end - start;
+            total_time += timeDiff;
+        }
+        total_times.push(total_time);
+    })
+    
+    // 요금
+    const calc_fee = (time) => {
+        const extra_charge = time < fees[0] ? 0 : Math.ceil((time - fees[0]) / fees[2]) * fees[3];
+        return fees[1] + extra_charge;
+    }
+    
+    return total_times.map(time => calc_fee(time));
 }
