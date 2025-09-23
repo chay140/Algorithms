4aa1aa0de01febe09cf7695e14afc5e7b833be54
Chay Park <chaihyunpark140@gmail.com>
2025-09-23 20:14:44 +0900

    [level 3] Title: 등굣길, Time: 7.33 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.js
+++ 프로그래머스/3/42898. 등굣길/등굣길.js
@@ -1,29 +1,23 @@
 function solution(m, n, puddles) {
-    let dp = Array.from({length: n}, () => new Array(m).fill(0));
-    // 시작점은 루프 돌리기 전에 지정
-    dp[0][0] = 1;
-
-    // 웅덩이 표시
-    for (let puddle of puddles) {
-        dp[puddle[1] - 1][puddle[0] - 1] = -1;
+    const MOD = 1_000_000_007;
+    const puddlesSet = new Set(puddles.map(puddle => `${puddle[0]},${puddle[1]}`));
+    const memo = {};
+    
+    const dp_top_down = (x, y) => {
+        // 베이스 케이스
+        if (x === 1 && y === 1) return 1;
+        if (x === 0 || y === 0) return 0;
+        
+        const key = `${x},${y}`
+        
+        if (key in memo) return memo[key];
+        if (puddlesSet.has(key)) return 0;
+        
+        memo[key] = (dp_top_down(x - 1, y) + dp_top_down(x, y - 1)) % MOD;
+        return memo[key];
     }
     
-    for (let i = 0; i < dp.length; i++) {
-        for (let j = 0; j < dp[i].length; j++) {
-            // 불필요한 연산 건너뛰기
-            if (dp[i][j] < 0 || (i === 0 && j === 0)) continue;
-            
-            // 왼쪽 경우의 수
-            if (j - 1 >= 0 && dp[i][j - 1] >= 0) {
-                dp[i][j] += dp[i][j - 1] % 1000000007;
-            }
-            // 위쪽 경우의 수
-            if (i - 1 >= 0 && dp[i - 1][j] >= 0) {
-                dp[i][j] += dp[i - 1][j] % 1000000007;
-            }
-            
-        }
-    }
-        
-    return dp[n - 1][m - 1] % 1000000007;
+    dp_top_down(m, n)
+
+    return memo[`${m},${n}`]
 }
