63b7f245b89290742997d79a24ca6c658e66f6dd
Chay Park <chaihyunpark140@gmail.com>
2025-09-27 23:48:16 +0900

    [level 2] Title: 타겟 넘버, Time: 108.55 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,12 +1,12 @@
 function solution(numbers, target) {
     // 타겟과 일치하는 결과수 저장
-    let count = 0;
+    let count = {};
     
     // 모든 조합을 만들어서 결과를 비교하기 위함
     function dfs(index, total) {
         // breakpoint
         if (index === numbers.length) {
-            if (total === target) count++;
+            count[total] = (count[total] || 0) + 1;
             return;
         }
         
@@ -17,6 +17,8 @@ function solution(numbers, target) {
     }
     
     dfs(0, 0);
+
+    // 몇개의 합을 만들 수 있는 문제였다면 count의 길이: Object.keys(count).length
     
-    return count;
+    return count[target];
 }
